c9b19b34ee4a0d12afe6c446bccfc29fed57e82a
elitaldev <eligoli1007@yahoo.co.uk>
2023-02-22 16:21:05 +0000

    fixed restart button and boxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,17 +9,25 @@ let currentPlayer = x_TEXT;
 let spaces = Array(9).fill(null);
 
 const startGame = () => {
-  boxes.forEach(box => box.addEventListener("click", boxClicked));
+    boxes.forEach(box => box.addEventListener("click", boxClicked));
 };
 
 function boxClicked(e) {
-  const id = e.target.id;
+    const id = e.target.id;
 
-  if (!spaces[id]) {
-    spaces[id] = currentPlayer;
-    e.target.innerText = currentPlayer;
-    currentPlayer = currentPlayer == x_TEXT ? o_TEXT : x_TEXT;
-  }
+    if (!spaces[id]) {
+        spaces[id] = currentPlayer;
+        e.target.innerText = currentPlayer;
+        currentPlayer = currentPlayer == x_TEXT ? o_TEXT : x_TEXT;
+    }
+}
+restartBtn.addEventListener("click", restart);
+function restart() {
+    spaces.fill(null);
+
+    boxes.forEach(box =>
+        box.innerText = "");
+    currentPlayer = x_TEXT;
 }
 
 startGame();
