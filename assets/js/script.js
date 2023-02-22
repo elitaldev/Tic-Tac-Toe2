2a795bf8a3b69a7705949c9309494334ee66ac91
elitaldev <eligoli1007@yahoo.co.uk>
2023-02-22 16:08:14 +0000

    initial javascript

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+let playerText = document.getElementById("playerText");
+let restartBtn = document.getElementById("restartBtn");
+let boxes = Array.from(document.getElementsByClassName("box"));
+
+const o_TEXT = "o";
+const x_TEXT = "x";
+let currentPlayer = x_TEXT;
+
+let spaces = Array(9).fill(null);
+
+const startGame = () => {
+  boxes.forEach(box => box.addEventListener("click", boxClicked));
+};
+
+function boxClicked(e) {
+  const id = e.target.id;
+
+  if (!spaces[id]) {
+    spaces[id] = currentPlayer;
+    e.target.innerText = currentPlayer;
+    currentPlayer = currentPlayer == x_TEXT ? o_TEXT : x_TEXT;
+  }
+}
+
+startGame();
